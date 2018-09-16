ab3a828c9aeaa5f61d1edebd9b42999b32306d48
Jliv316 <jclivingston316@gmail.com>
2018-09-16 15:51:33 -0600

    creates user show endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,12 @@ module Api::V1
   class UsersController < ApplicationController
     def create
       user = User.create(user_params)
+      render user, status: 201
+    end
+
+    def show
+      user = User.find_by(token: params[:user][:token])
+      render user, status: 200
     end
 
     private
