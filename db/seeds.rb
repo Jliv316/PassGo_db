ea9dcb86b90df5aaa01ad9700488711fb6b406e7
Jliv316 <jclivingston316@gmail.com>
2018-09-16 14:28:58 -0600

    changes to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(name: "Dr. Nick", email: "dNick@hotmail.com", token: ";ljasdfo;iuljmdk;21j")
+user = User.create(name: "Ray", email: "Ray@ray.com", token: "KbIFV2FF6lXS6LjZSoNeXZ5sLuu1")
 company = Company.create(name: "Slice Works", street: "1433 17th St", city: "Denver", state: "CO", zip_code: 80202, industry: "Pizza", lat: 39.7514038, lng: -104.9975539)
+Turing = Company.create(name: "Turing: School of Software and Design", street: "1331 17th St")
 UserCompany.create(user_id: user.id, company_id: company.id)
